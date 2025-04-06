aca1b5b59f52ec3ee3c012ce80ceb1107032c0a4
Himanshu Rai <himanshuraimau9@gmail.com>
2025-04-06 23:33:59 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ favorites/Readme.md
@@ -0,0 +1,28 @@
+# Project 1: Favorites Application
+
+This is a basic Anchor app using PDAs to store data for a user, and Anchor's account checks to ensure each user is only allowed to modify their own data.
+
+It's used by the [Solana Professional Education](https://github.com/solana-developers/professional-education) course.
+
+We recommend creating a separate github repository for each project and commiting your code as you follow along the video.
+
+## Favorites Program
+
+### Resources
+- [Anchor Documentation](https://www.anchor-lang.com/)
+- [Solana Documentation](https://docs.solana.com/)
+- [Solana Playground](https://beta.solpg.io/)
+- [Rust Documentation](https://doc.rust-lang.org/book/)
+
+### Running This Project
+We highly recommend creating your own github repository and building along with the video. This will help you learn the most and give you a reference to look back on later.
+
+If you want to check the final result of the project, you can clone this repository and run the following commands:
+
+```bash
+npm i
+anchor build
+anchor test
+```
+
+You should see the tests pass once completed.
--- favorites/programs/favorites/src/lib.rs
+++ favorites/programs/favorites/src/lib.rs
@@ -1,16 +1,65 @@
 use anchor_lang::prelude::*;
-
+// Our program's address!
+// This matches the key in the target/deploy directory
 declare_id!("3atXBCNGckTjdehYo6V4d6KHXf1mQfx1HzfW2LUremrK");
 
+// Anchor programs always use 8 bits for the discriminator
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+// Our Solana program! 
 #[program]
 pub mod favorites {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    // Our instruction handler! It sets the user's favorite number and color
+    pub fn set_favorites(context: Context<SetFavorites>, number: u64, color: String, hobbies: Vec<String>) -> Result<()> {
+        let user_public_key = context.accounts.user.key();
+        msg!("Greetings from {}", context.program_id);
+        msg!(
+            "User {user_public_key}'s favorite number is {number}, favorite color is: {color}",
+        );
+
+        msg!(
+            "User's hobbies are: {:?}",
+            hobbies
+        ); 
+
+        context.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies
+        });
         Ok(())
     }
+
+    // We can also add a get_favorites instruction handler to return the user's favorite number and color
 }
 
+// What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>
+}
+// When people call the set_favorites instruction, they will need to provide the accounts that will be modifed. This keeps Solana fast!
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+        seeds=[b"favorites", user.key().as_ref()],
+    bump)]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
